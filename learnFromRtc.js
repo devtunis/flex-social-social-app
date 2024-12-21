f943d8bafa1e26ed4fcf697027a3dac3e0bfc10c
Ghaith Nahdi <121437369+TUNISIA-user@users.noreply.github.com>
2024-12-21 07:21:30 -0500

    Create learnFromRtc.js
    Create learnFromRtc.js

---
+++ learnFromRtc.js
@@ -0,0 +1,10 @@
+  devices.forEach((item)=>{
+       const option = document.createElement('option')
+        if(item.kind=="audiooutput"){
+            console.log(item)
+
+        }
+    })
+
+
+this option create from js to render in the dom
