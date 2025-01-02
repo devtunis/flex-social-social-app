8b9215bdc9a3da5dcb6d66131fdb28df201d1423
Ghaith Nahdi <121437369+TUNISIA-user@users.noreply.github.com>
2025-01-02 01:56:39 +0100

    Update learnFromRtc.js

--- learnFromRtc.js
+++ learnFromRtc.js
@@ -8,3 +8,24 @@
 
 
 this option create from js to render in the dom
+
+
+
+ 
+let notifyBtn  = document.getElementById("notifyBtn")
+
+Notification.requestPermission().then((permission) => {
+    if (permission !== "granted") {
+        return; // Stop if the permission is not granted
+    }
+
+    // Create the notification only if permission is granted
+    const notification = new Notification("To-Do List", {
+        body: "You have an unread message!",
+    });
+
+    // Optionally, you can add event listeners to the notification
+    notification.onclick = () => {
+        console.log("Notification clicked!");
+    };
+});
