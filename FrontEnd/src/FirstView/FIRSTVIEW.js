e0f8861ae64a180c4ec1e9f1b981410bc344281e
SAFIR jHanm🔥 <121437369+TUNISIA-user@users.noreply.github.com>
2025-07-20 10:10:53 +0100

    Update FIRSTVIEW.js

--- FrontEnd/src/FirstView/FIRSTVIEW.js
+++ FrontEnd/src/FirstView/FIRSTVIEW.js
@@ -1,7 +1,9 @@
-import React from 'react'
+
+
+import React ,{useState,useEffect}from 'react'
 import "./FirstVIew.css"
 import { useNavigate } from 'react-router-dom'
-
+ 
 
 const   FIRSTVIEW = () => {
     const Nav = useNavigate()
@@ -10,26 +12,25 @@ const   FIRSTVIEW = () => {
   return (
     <> 
  
-   
+    
 
-    <div className='container__first_view'>
+    <div className='container__first_view' style={{backgroundColor:"black",height:"100vh"}}>
         
         <div className="insdide__cotnainer">
-          <div className='firstview__img'><img src='./betterfly.PNG'/>
-          
-          <ButterFlyD/></div>
-          
+          <div className='firstview__img' style={{border:"3px solid white",borderRadius:"100px",padding:"3px"}}><img src='./imgStartup/imgStartup.png'
+          className='rotate--img--home'
+          style={{width: "90px",height:"90px",borderRadius:"100px"}}/></div>
           <div className='firstview__titile'> 
-            <h1 className='ASKENET'>ASKE NET</h1>
-            <h4 className='whatsapp'>What's up?     </h4>
+            <h1 className='ASKENET' style={{color:"white"}}>FLEX</h1>
+            <h4 className='whatsapp'  color='gold'>What's up?     </h4>
           </div>
 
 
           <div className='firstview__button'>
 
-            <input type='button' className='button1' value="Create a new account" onClick={()=> {Nav("/login")}} />
+            <input type='button'  className='button1' value="Create a new account" onClick={()=> {Nav("/login")}} />
 
-            <input type='button' className='button2' value="Sign in"  onClick={()=>{Nav("/auth")}} />
+            <input type='button' style={{color:"#000"}} className='button2' value="Sign in"  onClick={()=>{Nav("/auth")}} />
            
           </div>          
 
